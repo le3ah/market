84fe03e1a0ea2d1ae7873fd14e19a201a2cd87f4
Leah Miller <lkmiller.87@gmail.com>
2018-11-03 10:05:09 -0600

    Add final tests and methods for iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -4,4 +4,20 @@ class Market
     @name = name
     @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(food)
+    @vendors.find_all do |vendor|
+    vendor.inventory.has_key?(food)
+    end 
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -16,6 +16,56 @@ class MarketTest < Minitest::Test
 
   def test_it_starts_with_no_vendors
     market = Market.new("South Pearl Street Farmers Market")
-    assert_equal [], market.vendors 
+    assert_equal [], market.vendors
+  end
+
+  def test_it_can_add_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    assert_equal 3, market.vendors.count
+    assert_instance_of Vendor, market.vendors.first
+  end
+
+  def test_it_can_find_vendor_names
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], market.vendor_names
+  end
+
+  def test_it_can_find_vendors_who_sell_peaches
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    assert_equal [vendor_1, vendor_3], market.vendors_that_sell("Peaches")
+    assert_equal [vendor_2], market.vendors_that_sell("Banana Nice Cream")
   end
 end
